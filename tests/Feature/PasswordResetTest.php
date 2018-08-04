3adc8100e87b3d0129626a4e63782dfa5b9d10c0
Brice Hartmann <bricejhartmann@gmail.com>
2018-08-04 00:03:28 +0000

    PasswordResetTest imports, WithFaker trait, and constants

--- tests/Feature/PasswordResetTest.php
+++ tests/Feature/PasswordResetTest.php
@@ -2,19 +2,22 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
+use App\User;
+use Hash;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Notification;
+use Password;
+use Tests\TestCase;
 
 class PasswordResetTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+    use WithFaker;
+
+    const ROUTE_PASSWORD_EMAIL = 'password.email';
+    const ROUTE_PASSWORD_REQUEST = 'password.request';
+    const ROUTE_PASSWORD_RESET = 'password.reset';
+    const ROUTE_PASSWORD_RESET_SUBMIT = 'password.reset.submit';
+
+    const USER_ORIGINAL_PASSWORD = 'secret';
 }
