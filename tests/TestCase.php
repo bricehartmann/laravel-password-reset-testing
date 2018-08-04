660b08295a9fd6023e6fafcd75f7700004b82860
Brice Hartmann <bricejhartmann@gmail.com>
2018-08-04 00:22:15 +0000

    use database transactions and fake notifications in base test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Notification;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseTransactions;
+
+    /**
+     * Set up the test case.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Notification::fake();
+    }
 }
